739839e946e20db84ce3db57f9a7dbcf8f112d45
trungnt89 <46738728+trungnt89@users.noreply.github.com>
2021-12-08 10:15:47 +0900

    add INSERT,LIST sql

--- src/index.php
+++ src/index.php
@@ -1,5 +1,4 @@
 <?php
-echo "HelloWorld!";
 $HOSTNAME = "db";
 $USERNAME = "root";
 $PASSWORD = "example";
@@ -9,6 +8,21 @@ $conn = new mysqli($HOSTNAME, $USERNAME, $PASSWORD, $DATABASE);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } else {
-    echo "Connected to MySQL server successfully!!!";
+    //echo "Connected to MySQL server successfully!!!";
 }
+
+
+$sql = "INSERT INTO users (name) VALUES('TEST_".date("Y/m/d_h:i:s a", time())."')";
+$result = $conn->query($sql);
+
+$sql = 'SELECT * FROM users';
+if ($result = $conn->query($sql)) {
+    while ($data = $result->fetch_object()) {
+        $users[] = $data;
+    }
+}
+
+echo json_encode($users);
+$conn->close();
+
 ?>
